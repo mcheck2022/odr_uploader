6d1807fbc6ce267ce5d6eb9f02561e28625ebfbd
Magno C. Heck <magno.cardona@mercadolibre.cl>
2023-01-27 15:05:12 -0300

    [CHG] Pines Default - No Disponible upload

--- setup-assets.sh
+++ setup-assets.sh
@@ -11,9 +11,9 @@ DG_KEY_ASSETS=(
 )
 
 DG_DIC_ASSETS=(
-    ['Red']='cashmaps_libreriared_icon_pin'
-    ['Bonafide']='cashmaps_bonafide_icon_pin'
-    ['Mariano Max']='cashmaps_marianomax_icon_pin'
+    ['Red']='cashmaps_libreriared_icon_pin_notavailable'
+    ['Bonafide']='cashmaps_bonafide_icon_pin_notavailable'
+    ['Mariano Max']='cashmaps_marianomax_icon_pin_notavailable'
 )
 
 #######################################################################################################################
