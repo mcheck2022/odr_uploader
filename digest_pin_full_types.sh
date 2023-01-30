c319cb4ae1cd47a9000ede2829ae638c65a35b2f
Magno C. Heck <magno.cardona@mercadolibre.cl>
2023-01-30 14:21:12 -0300

    [CHG] test upload final script

--- digest_pin_full_types.sh
+++ digest_pin_full_types.sh
@@ -70,10 +70,10 @@ setup_dg () {
 
 upload-odr-create-images () {
     echo "DG upload-odr-create-images with argument at: $1"
-    #echo "odr $ODR_TYPE image --directory $d"
-    #RESULTLOG=$(odr $ODR_TYPE image --directory $d)
-    #echo $RESULTLOG
-    #echo $RESULTLOG >> $DG_ODR_RESULT_LOG
+    echo "odr $ODR_TYPE image --directory $d"
+    RESULTLOG=$(odr $ODR_TYPE image --directory $d)
+    echo $RESULTLOG
+    echo $RESULTLOG >> $DG_ODR_RESULT_LOG
 }
 
 upload-directories () {
