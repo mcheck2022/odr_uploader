43242e292ed8f6c79f6573b01e96fb581548c1fc
Magno C. Heck <magno.cardona@mercadolibre.cl>
2023-02-13 14:25:09 -0300

    [CHG] fix asset

--- setup-pins-assets.sh
+++ setup-pins-assets.sh
@@ -4,69 +4,16 @@ source ./base.sh
 
 declare -a DG_KEY_ASSETS=(
     'Mercado Express'
-    'Rapipago'
-    'Pago Fácil'
-    'Cobro Express'
-    'Dr Ahorro'
-    'Hausbrot'
-    'Cartu124'
-    'Mariano Max'
-    'Pintecord'
-    'Carrefour'
-    'Dia'
-    'Libertad'
-    'Axion'
-    'Farmaplus'
-    'Coto'
-    'Farmacia Líder'
-    'Red'
-    'Link'
-    'Diarco'
-    'Maxiconsumo'
-    'Cooperativa Obrera'
-    'La anonima'
-    'Starbucks'
-    'Mostaza'
-    'Bonafide'
-    'Beer Markert'
-    'Banelco'
 )
-
 declare -A DG_DIC_ASSETS=(
-    ['Mercado Express']='cashmaps_libreriared'
-    ['Rapipago']='cashmaps_rapipago'
-    ['Cobro Express']='cashmaps_cobroexpress'
-    ['Pago Fácil']='cashmaps_pagofacil'
-    ['Dr Ahorro']='cashmaps_doctorahorro'
-    ['Hausbrot']='cashmaps_hausbrot'
-    ['Cartu124']='cashmaps_kiosko724'
-    ['Mariano Max']='cashmaps_marianomax'
-    ['Pintecord']='cashmaps_pintecord'
-    ['Carrefour']='cashmaps_carrefour'
-    ['Dia']='cashmaps_dia'
-    ['Libertad']='cashmaps_libertad'
-    ['Axion']='cashmaps_axion'
-    ['Farmaplus']='cashmaps_farmaplus'
-    ['Coto']='cashmaps_coto'
-    ['Farmacia Líder']='cashmaps_farmacialider'
-    ['Red']='cashmaps_libreriared'
-    ['Link']='cashmaps_link'
-    ['Diarco']='cashmaps_diarco'
-    ['Maxiconsumo']='cashmaps_maxiconsumo'
-    ['Cooperativa Obrera']='cashmaps_cooperativaobrera'
-    ['La anonima']='cashmaps_laanonima'
-    ['Starbucks']='cashmaps_starbucks'
-    ['Mostaza']='cashmaps_mostaza'
-    ['Bonafide']='cashmaps_bonafide'
-    ['Beer Markert']='cashmaps_beermarkert'
-    ['Banelco']='cashmaps_banelco'
+    ['Mercado Express']='cashmaps_mpexpress'
 )
 
 #######################################################################################################################
 # ODR_TYPE = create | update
 export ODR_TYPE='update'
 # SKIP_REAL_ODR_UPLOAD = yes | no
-export SKIP_REAL_ODR_UPLOAD='yes'
+export SKIP_REAL_ODR_UPLOAD='no'
 
 
 #######################################################################################################################
